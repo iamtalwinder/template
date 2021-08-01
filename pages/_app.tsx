afaca8d7afcd9fd6883823feee86731169e33847
iamtalwinder <singhtalwinder790@gmail.com>
2021-08-01 15:15:58 +0530

    :tada: Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { AppProps } from 'next/app';
+import { defaultTheme } from '../src/theme';
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ src/theme/default.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+export const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
